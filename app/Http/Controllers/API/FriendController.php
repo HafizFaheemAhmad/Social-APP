27987b1f63985171a05298e743ca18043a0b59e9
faheem <faheem@gmail.com>
2021-11-19 10:18:12 +0500

    third commit

--- app/Http/Controllers/API/FriendController.php
+++ app/Http/Controllers/API/FriendController.php
@@ -4,44 +4,38 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
 use App\Models\Friend;
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\key;
 
 class FriendController extends Controller
 {
-
     public function addFriend(Request $req)
     {
-        $key = $req->token;
         $fid = $req->fid;
-        $data = DB::table('users')
-            ->where('jwt_token', $key)->get();
-        $id = $data[0]->id;
-        if ($id != $fid) {
-            if (DB::table('users')->where('id', $fid)->exists()) {
-                if (DB::table('friends_request')
-                    ->where(['userid_1' => $id, 'userid_2' => $fid])
-                    ->orwhere(['userid_1' => $fid, 'userid_2' => $id])
+        $token = request()->bearerToken();
+        try {
+            $decoded_data = JWT::decode($token, new Key('example', 'HS256'));
+            User::where("jwt_token", $token)->first();
+            if ($decoded_data->data->id != $fid) {
+                if (Friend::where(['userid_1' => $decoded_data->data->id, 'userid_2' => $fid])
+                    ->orwhere(['userid_1' => $fid, 'userid_2' => $decoded_data->data->id])
                     ->doesntExist()
                 ) {
-                    $numrows = count($data);
-                    if ($numrows > 0) {
-                        $friend = new Friend;
-                        $friend->userid_1 = $id;
-                        $friend->userid_2 = $fid;
-                        $friend->save();
-                        return response()->json(["messsage" => "you are friend now of" . $fid]);
-                    } else {
-                        return response()->json(["messsage" => "Currently you are not login"]);
-                    }
+                    $friend = new Friend;
+                    $friend->userid_1 = $decoded_data->data->id;
+                    $friend->userid_2 = $fid;
+                    $friend->save();
+                    return response()->json(["messsage" => "you are friend now of" . $fid]);
                 } else {
                     return response(["message" => "User with id = " . $fid . " is already your friend"]);
                 }
             } else {
-                return response(["message" => "User with id = " . $fid . " is not registerd on our application"]);
+                return response(["message" => "you are not allow to be friend of yourself"]);
             }
-        } else {
-            return response(["message" => "you are not allow to be friend of yourself"]);
+        } catch (\Exception $ex) {
+            return $ex->getMessage();
         }
     }
 }
